13f60b71eea1f1a2de411720d37ea563ca2a5c09
Lilit <baregamyanlilit36@gmail.com>
2026-06-02 19:32:51 +0400

    first commit

---
+++ data/data.tsx
@@ -0,0 +1,70 @@
+import { FaChurch, FaUtensils } from "react-icons/fa";
+export const envelop=[
+  '/envelope.png',
+  '/forenvelope.png'
+]
+export const programImgs=['/ekexeci.jpg','/restoran.jpg']
+export const program = [
+  {
+    src: "/loc1.png",
+    time: "11:00",
+    title: "Փեսայի տուն",
+    address: "Գուսան Շերամ 39",
+  },
+  {
+    src: "/loc2.png",
+    time: "13:00",
+    title: "Հարսնացուի տուն",
+    address: "Զավարյան 36",
+  },
+  {
+    icon: <FaChurch />,
+    time: "15:30",
+    title: "Պսակադրություն",
+    address: "Սուրբ Գրիգոր Լուսավորիչ եկեղեցի",
+  },
+  {
+    icon: <FaUtensils />,
+    time: "17:00",
+    title: "Հարսանյաց հանդիսություն",
+    address: "Աստաֆյան ռեստորանային համալիր",
+  },
+];
+export const firstname = ["Էդմոնդ և Կարինե"];
+export const name = ["ԷԴՄՈՆԴ", "ԿԱՐԻՆԵ"];
+export const date = "28/08/2026";
+
+export const dateTimeBox: any = new Date("2026-08-28T11:00:00");
+
+export const calendar: any = [
+  ["Ուրբաթ", "Շաբաթ", "Կիրակի"],
+  ["28", "29", "30"],
+];
+
+export const txt1 =
+  "Սիրով հրավիրում ենք \n Ձեզ, մեզ հետ տոնելու\n մեր կյանքի\n ամենակարևոր\n օրը ՝ մեր \n հարսանեկան \nարարողությունը ";
+export const txt1_1 =
+  "Դուք հրավիրված եք մեր Հարսանիքին \nՕր որտեղ հիմք կդրվի\n Նոր ընտանիքի";
+export const txt2 =
+  "Խնդրում ենք \nհարսանիքի օրը \nԽՈՒՍԱՓԵԼ \nսպիտակ հագուստ\nկրելուց\n\nՄնացած \nբոլոր երանգները\nսազում են Ձեզ \n առավել քան երբևէ";
+
+export const txt3 = "Ինչ Աստված \n միացրեց,մարդը\nթող չբաժանի\n\nՄարկոս 10:9";
+export const txt4 = "ՕՐ ՈՐԻՑ ՀԵՏՈ \ԵՐԿՈՒ ՏԱՐԲԵՐ\n ԱՇԽԱՐՀՆԵՐ \nՄԻԱՎՈՐՎԵԼՈՎ ԿՍՏԵՂԾԵՆ\n ՄԵԿ \nՏՈՒՆ ՈՒ ՏԱՆԻՔ  ";
+export const vazox1 ='Սիրում եմ Սիրում եմ Սիրում եմ Սիրում եմ Սիրում եմ Սիրում եմ Սիրում Սիրում եմ Սիրում եմ Սիրում եմ Սիրում եմ Սիրում եմ Սիրում եմ Սիրում Սիրում եմ Սիրում եմ Սիրում եմ Սիրում եմ Սիրում եմ Սիրում եմ Սիրում Սիրում եմ Սիրում եմ Սիրում եմ Սիրում եմ Սիրում եմ Սիրում եմ Սիրում Սիրում եմ Սիրում եմ Սիրում եմ Սիրում եմ Սիրում եմ Սիրում եմ Սիրում Սիրում եմ Սիրում եմ Սիրում եմ Սիրում եմ Սիրում եմ Սիրում եմ Սիրում Սիրում եմ Սիրում եմ Սիրում եմ Սիրում եմ Սիրում եմ Սիրում եմ Սիրում Սիրում եմ Սիրում եմ Սիրում եմ Սիրում եմ Սիրում եմ Սիրում եմ Սիրում Սիրում եմ Սիրում եմ Սիրում եմ Սիրում եմ Սիրում եմ Սիրում եմ Սիրում Սիրում եմ Սիրում եմ Սիրում եմ Սիրում եմ Սիրում եմ Սիրում եմ Սիրում';
+export const vazox2 ='I LOVE YOU I LOVE YOU I LOVE YOU I LOVE YOU I LOVE YOU I LOVE YOU I LOVE YOU I LOVE YOU I LOVE YOU I LOVE YOU I LOVE YOU I LOVE YOU I LOVE YOU I LOVE YOU I LOVE YOU I LOVE YOU I LOVE YOU I LOVE YOU I LOVE YOU I LOVE YOU I LOVE YOU I LOVE YOU I LOVE YOU I LOVE YOU I LOVE YOU I LOVE YOU I LOVE YOU I LOVE YOU I LOVE YOU I LOVE YOU I LOVE YOU I LOVE YOU I LOVE YOU I LOVE YOU I LOVE YOU I LOVE YOU I LOVE YOU I LOVE YOU I LOVE YOU I LOVE YOU I LOVE YOU I LOVE YOU I LOVE YOU I LOVE YOU I LOVE YOU I LOVE YOU I LOVE YOU I LOVE YOU I LOVE YOU I LOVE YOU I LOVE YOU I LOVE YOU I LOVE YOU I LOVE YOU ILOVE YOU I LOVE YOU';
+export const anim: any = {
+  initial: { opacity: 0, y: 40 },
+  whileInView: {
+    opacity: 1,
+    y: 0,
+    transition: {
+      duration: 0.5,
+      ease: "easeIn",
+    },
+  },
+  viewport: { once: true },
+};
+
+
+export const rsvp =
+  "";
